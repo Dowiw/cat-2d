907fac32d4c092f8e5a4733a47b42de11da85003
Dowiw <monjie12272@gmail.com>
2024-11-12 22:01:19 +0100

    more code

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 ## randomly generated farming game
 
 
-with help from RyiSnow's tutorial: ["How to Make a 2D Game in Java"](https://www.youtube.com/watch?v=om59cwR7psI).
+with help from (my first LEGEND) RyiSnow's tutorial: ["How to Make a 2D Game in Java"](https://www.youtube.com/watch?v=om59cwR7psI).
--- src/main/Main.java
+++ src/main/Main.java
@@ -9,10 +9,15 @@ public class Main {
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setTitle("2D Farmer!");
+        
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
 
         window.setLocationRelativeTo(null);
-        window.setVisible(true);
+        window.setVisible(true); 
 
-        
+        gamePanel.startGameThread();
     }
 }
