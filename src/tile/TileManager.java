f29a60702f8f8c6a01c3738e9899b8a1cc1e3c2e
Dowiw <monjie12272@gmail.com>
2024-11-17 13:52:22 +0100

    made render to worldmap

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -102,17 +102,16 @@ public final class TileManager {
             int screenY = worldY - gp.player.worldY + gp.player.screenY;
 
             //Render only near Player
-            if(worldX > gp.player.worldX - gp.player.screenX &&
-                worldX < gp.player.worldX + gp.player.screenX &&
-                worldY > gp.player.worldY - gp.player.screenY &&
-                worldY < gp.player.worldY + gp.player.screenY) {
-
-            g2.drawImage(tile[tileNum].image, screenX, screenY, gp.tileSize, gp.tileSize, null);
-            worldCol++;
-            
+            if (worldX + gp.tileSize > gp.player.worldX - gp.player.screenX && 
+                worldX - gp.tileSize < gp.player.worldX + gp.player.screenX &&
+                worldY + gp.tileSize > gp.player.worldY - gp.player.screenY && 
+                worldY - gp.tileSize < gp.player.worldY + gp.player.screenY) {
+                    g2.drawImage(tile[tileNum].image, screenX, screenY, gp.tileSize, gp.tileSize, null);
                 }
 
-            if(worldCol == gp.maxWorldCol) {
+            worldCol++;
+
+            if (worldCol == gp.maxWorldCol) {
                 worldCol = 0;
                 worldRow++;
             }
