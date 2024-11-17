8cd7453d7ba0145c3940852cb2b8b82a23d26736
Dowiw <monjie12272@gmail.com>
2024-11-17 12:50:46 +0100

    made naming tiles organized

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -33,19 +33,19 @@ public final class TileManager {
             tile[0].image = ImageIO.read(getClass().getResourceAsStream("/res/tiles/grass.png"));
 
             tile[1] = new Tile();
-            tile[1].image = ImageIO.read(getClass().getResourceAsStream("/res/tiles/tree.png"));
+            tile[1].image = ImageIO.read(getClass().getResourceAsStream("/res/tiles/sand.png"));
 
             tile[2] = new Tile();
-            tile[2].image = ImageIO.read(getClass().getResourceAsStream("/res/tiles/woodfloor.png"));
+            tile[2].image = ImageIO.read(getClass().getResourceAsStream("/res/tiles/tree.png"));
 
             tile[3] = new Tile();
-            tile[3].image = ImageIO.read(getClass().getResourceAsStream("/res/tiles/woodwall.png"));
+            tile[3].image = ImageIO.read(getClass().getResourceAsStream("/res/tiles/water.png"));
 
             tile[4] = new Tile();
-            tile[4].image = ImageIO.read(getClass().getResourceAsStream("/res/tiles/sand.png"));
+            tile[4].image = ImageIO.read(getClass().getResourceAsStream("/res/tiles/woodfloor.png"));
 
             tile[5] = new Tile();
-            tile[5].image = ImageIO.read(getClass().getResourceAsStream("/res/tiles/water.png"));
+            tile[5].image = ImageIO.read(getClass().getResourceAsStream("/res/tiles/woodwall.png"));
 
         } catch (IOException e) {
             e.printStackTrace();
